db0e46e7a3161dec4d922fdb503b0242b9bf662f
Alexander Zheglov <iamdonefor@yandex.ru>
2021-12-14 12:37:57 +0300

    part2

--- day14/code.cpp
+++ day14/code.cpp
@@ -3,29 +3,47 @@ using namespace std;
 
 using TPolyMap = map<pair<char, char>, char>;
 
-string polymerize(const string& poly, const TPolyMap& m) {
-    string result;
-    result.reserve(poly.size() * 2);
+vector<int64_t> polymerize(const string& poly, const TPolyMap& m, int steps) {
+    map<pair<char, char>, int64_t> poly_pairs;
 
-    for (int i=1; i < poly.size(); ++i) {
-        result.push_back(poly[i-1]);
-        result.push_back(m.at({poly[i-1], poly[i]}));
+    for (int i=1; i<poly.size(); ++i) {
+        ++poly_pairs[{poly[i-1], poly[i]}];
     }
 
-    result.push_back(poly.back());
-    return result;
+    for (int i=0; i<steps; ++i) {
+        map<pair<char, char>, int64_t> next_poly_pairs;
+
+        for (const auto& [p, count] : poly_pairs) {
+            auto midc = m.at(p);
+            next_poly_pairs[{p.first, midc}] += count;
+            next_poly_pairs[{midc, p.second}] += count;
+        }
+
+        int64_t s{0};
+        for (const auto it : next_poly_pairs) {
+            s += it.second;
+        }
+        cout << "length after step: " << i + 1 << " = " << s + 1 << endl;
+
+        poly_pairs = move(next_poly_pairs);
+    }
+
+    vector<int64_t> counter(26);
+    for (const auto& it : poly_pairs) {
+        counter[it.first.first - 'A'] += it.second;
+        counter[it.first.second - 'A'] += it.second;
+    }
+
+    return counter;
 }
 
-int part1(const string& poly, const TPolyMap& m) {
-    string result{poly};
-    for (int i=0; i<10; ++i) {
-        result = polymerize(result, m);
-        cout << i+1 << " : " << result.size() << " " << result << endl;
-    }
+int64_t solve(const string& poly, const TPolyMap& m, int steps=10) {
+    auto counter = polymerize(poly, m, steps);
+    counter[poly.front() - 'A'] += 1;
+    counter[poly.back() - 'A'] += 1;
 
-    vector<int> counter(26, 0);
-    for (const auto c : result) {
-        counter[c - 'A'] += 1;
+    for (auto& i : counter) {
+        i /= 2;
     }
 
     auto newend = remove(counter.begin(), counter.end(), 0);
@@ -72,14 +90,16 @@ CC -> N
 CN -> C)==="};
 
     auto [poly, m] = parse_input(input_stream);
-    auto result1 = part1(poly, m);
+    auto result1 = solve(poly, m);
+    auto result2 = solve(poly, m, 40);
 
-    return result1 == 1588;
+    return result1 == 1588 && result2 == 2188189693529;
 }
 
 int main() {
     assert(verify());
 
     auto [poly, m] = parse_input(cin);
-    cout << "part1: " << part1(poly, m) << endl;
+    cout << "part1: " << solve(poly, m) << endl;
+    cout << "part2: " << solve(poly, m, 40) << endl;
 }
