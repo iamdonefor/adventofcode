f374144dfef17ad5bedd0f6d57d42f33bc80994e
Alexander Zheglov <iamdonefor@yandex.ru>
2022-12-08 14:37:03 +0300

    08

--- skeleton.cpp
+++ skeleton.cpp
@@ -6,6 +6,7 @@ tinput parseInput(istream& is) {
     tinput result;
 
     for (string s; getline(is, s); ) {
+        if (s.empty()) { continue; }
         // if (sscanf(s.data(), "%d,%d", &x, &y) == 2)
     }
 
