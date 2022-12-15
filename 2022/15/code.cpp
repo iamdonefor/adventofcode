67990af78f83b0bbb475a0e1439bf6b2c074d155
Alexander Zheglov <iamdonefor@yandex.ru>
2022-12-15 17:48:42 +0300

    part2, manual answer

--- 2022/15/code.cpp
+++ 2022/15/code.cpp
@@ -13,15 +13,23 @@ public:
     }
 
     bool intersects(const trange& other) const {
-        if ( (*this)[0] <= other[1] || other[0] <= (*this)[1] ) {
+        if ( ((*this)[0] <= other[1] && (*this)[1] >= other[0])
+            || (other[0] <= (*this)[1] && other[1] >= (*this)[0])) {
             return true;
         }
         return false;
     }
 
     trange combine(const trange& other) const {
-        assert(intersects(other));
         return trange(min((*this)[0], other[0]),  max((*this)[1], other[1]));
+    }
+
+    bool contains(int64_t x) const {
+        return x >= (*this)[0] && x <= (*this)[1];
+    }
+
+    bool contains(const trange& other) const {
+        return other[0] >= (*this)[0] && other[1] <= (*this)[1];
     }
 
     int64_t size() const { return (*this)[1] - (*this)[0] + 1; }
@@ -47,6 +55,8 @@ public:
         return trange(self_[1] - width, self_[1] + width);
     }
 
+    tcoords beacon() const { return beacon_; }
+
     friend ostream& operator<< (ostream& os, const tscaner& s) {
         os << "[" << s.self_[0] << ", " << s.self_[1] << "](" << s.range_ << ")";
         return os;
@@ -59,6 +69,7 @@ private:
 
 using tinput = vector<tscaner>;
 using tscanners = tinput;
+using tranges = vector<trange>;
 
 tinput parse_input(istream&& is) {
     tinput result;
@@ -88,12 +99,12 @@ tinput parse_input(istream& is) {
     return parse_input(move(is));
 }
 
-vector<trange> find_all_invalid(const tscanners& scanners, int64_t y) {
-    vector<trange> ranges;
+tranges find_all_invalid(const tscanners& scanners, int64_t y) {
+    tranges ranges;
 
     for (const auto& scanner : scanners) {
         if (const auto range = scanner.get_at_distance(y)) {
-            cout << scanner << " " << (*range)[0] << ", " << (*range)[1] << endl;
+            // cout << scanner << " " << (*range)[0] << ", " << (*range)[1] << endl;
             ranges.push_back(*range);
         }
     }
@@ -101,30 +112,79 @@ vector<trange> find_all_invalid(const tscanners& scanners, int64_t y) {
     return ranges;
 }
 
+tranges collapse_ranges(tranges ranges) {
+    sort(ranges.begin(), ranges.end(), [](const auto& l, const auto& r) {
+        if (l[0] < r[0]) { return true; }
+        if (l[0] > r[0]) { return false; }
+        return l.size() > r.size();
+    });
+
+    auto lit = ranges.begin();
+    for (auto cit = next(ranges.begin()) ; cit != ranges.end(); ++cit) {
+        if (lit->intersects(*cit) || (*lit)[1] + 1 == (*cit)[0]) {
+            *lit = lit->combine(*cit);
+        } else {
+            *(++lit) = *cit;
+        }
+    }
+    ranges.erase(next(lit), ranges.end());
+    return ranges;
+}
+
+int64_t find_beacons_in_ranges(const tscanners& scanners, const tranges& ranges, int64_t y) {
+    set<tcoords> beacons;
+    for (const auto& scanner : scanners) {
+        beacons.insert(scanner.beacon());
+    }
+    return accumulate(beacons.begin(), beacons.end(), 0ll,
+        [y, &ranges](const auto prev, const auto& beacon) {
+            if (beacon[0] != y) {
+                return prev;
+            }
+
+            for (const auto& r : ranges) {
+                if (r.contains(beacon[1])) {
+                    return prev + 1;
+                }
+            }
+
+            return prev;
+        }
+    );
+}
+
 int64_t solve(const tscanners& scanners, int64_t y) {
     auto ranges = find_all_invalid(scanners, y);
+
     if (ranges.size() == 1) {
         return ranges.front().size();
     }
 
-    sort(ranges.begin(), ranges.end(), [](const auto& l, const auto& r) {
-        if (l[0] < r[0]) { return true; }
-        return l.size() > r.size();
-    });
+    ranges = collapse_ranges(move(ranges));
+    const auto correction = find_beacons_in_ranges(scanners, ranges, y);
 
-    auto cit = next(ranges.begin());
-    auto lit = ranges.begin();
-    for (; cit != ranges.end(); ++cit) {
-        if (lit->intersects(*cit)) {
-            *lit = lit->combine(*cit);
-        } else {
-            ++lit;
+    return -correction + accumulate(ranges.begin(), ranges.end(), 0ll,
+        [](auto prev, const auto& cur) {
+            return prev + cur.size();
+    });
+}
+
+int64_t solve2(const tscanners& scanners, int64_t from, int64_t to) {
+    for (int64_t i=from; i <= to; ++i) {
+        const auto r = collapse_ranges(find_all_invalid(scanners, i));
+        const auto it = find_if(r.begin(), r.end(), [to](const auto& rr) {
+            return rr.contains(trange(0, to));
+        });
+
+        if (it == r.end()) {
+            cout << i << " ";
+            for (const auto& rr : r) {
+                cout << "(" << rr[0] << ", " << rr[1] << "), ";
+            } cout << endl;
         }
     }
 
-    return accumulate(ranges.begin(), next(lit), 0, [](auto prev, const auto& cur) {
-        return prev + cur.size();
-    });
+    return 100;
 }
 
 stringstream test{R"(Sensor at x=2, y=18: closest beacon is at x=-2, y=15
@@ -144,6 +204,9 @@ Sensor at x=20, y=1: closest beacon is at x=15, y=3
 )"};
 
 int main() {
-    cout << solve(parse_input(test), 10) << endl;
-    cout << solve(parse_input(fstream("input")), 2000000) << endl;
+    // cout << solve(parse_input(test), 10) << endl;
+    // cout << solve(parse_input(fstream("input")), 2000000) << endl;
+
+    cout << solve2(parse_input(test), 0, 20) << endl;
+    cout << solve2(parse_input(fstream("input")), 0, 4000000) << endl;
 }
